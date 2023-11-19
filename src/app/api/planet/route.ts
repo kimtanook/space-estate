b93b20501a1df8df5d8330c4a6c1a4ae3f176198
kim in seob <seob6402@gmail.com>
2023-11-19 20:01:46 +0900

    update: api

--- src/app/api/planet/route.ts
+++ src/app/api/planet/route.ts
@@ -3,14 +3,14 @@ import {NextResponse} from "next/server";
 // 초기 행성 데이터
 let planetPriceData = {
   planets: [
-    {name: "Mercury", price: "200,000,000,000"},
-    {name: "Venus", price: "100,000,000,000"},
-    {name: "Earth", price: "120,000,000,000"},
-    {name: "Mars", price: "340,000,000,000"},
-    {name: "Jupiter", price: "130,000,000,000"},
-    {name: "Saturn", price: "150,000,000,000"},
-    {name: "Uranus", price: "90,000,000,000"},
-    {name: "Neptune", price: "20,000,000,000"},
+    {name: "Mercury", price: 200},
+    {name: "Venus", price: 100},
+    {name: "Earth", price: 120},
+    {name: "Mars", price: 340},
+    {name: "Jupiter", price: 130},
+    {name: "Saturn", price: 150},
+    {name: "Uranus", price: 90},
+    {name: "Neptune", price: 20},
   ],
 };
 
@@ -20,12 +20,12 @@ setInterval(() => {
   planetPriceData.planets.forEach((planet) => {
     // 50% 확률로 가격을 10% 증가시키거나 감소시킴
     if (Math.random() < 0.5) {
-      planet.price = String(Number(planet.price) * 1.1); // 10% 증가
+      planet.price = Math.floor(planet.price * 1.1); // 10% 증가
     } else {
-      planet.price = String(Number(planet.price) * 0.9); // 10% 감소
+      planet.price = Math.floor(planet.price * 0.9); // 10% 감소
     }
   });
-}, 60000); // 1분(60초)마다 실행
+}, 5000);
 
 export async function GET() {
   return NextResponse.json(planetPriceData);
