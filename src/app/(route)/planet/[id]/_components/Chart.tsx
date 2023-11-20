d814035edfdc20221c696f977b1e3272ca0a47e0
kim in seob <seob6402@gmail.com>
2023-11-20 22:18:03 +0900

    update: chart date

--- src/app/(route)/planet/[id]/_components/Chart.tsx
+++ src/app/(route)/planet/[id]/_components/Chart.tsx
@@ -1,12 +1,15 @@
 import c3 from "c3";
 import "c3/c3.css"; // c3.js의 CSS 스타일을 가져옵니다.
+import {format} from "date-fns";
 import {useEffect} from "react";
 import styled from "styled-components";
 
 function Chart({chartData}: any) {
+  const priceData = chartData.map((item: any) => item.price);
+  const dateData = chartData.map((item: any) => item.date);
   const data: any = {
     // 그래프를 그리기 위한 데이터 (여기서는 최저/최고/평균)
-    columns: [chartData],
+    columns: [["price", ...priceData]],
     // 각 데이터의 type을 지정
     types: {
       // 최저, 최고의 경우 line 차트, 평균의 경우 bar 차트로 지정
@@ -21,11 +24,16 @@ function Chart({chartData}: any) {
         // X 축과 Y축에 대한 설정
         x: {
           label: "Date", // X 축의 라벨
-          categories: chartData.map((item: any, index: number) => index), // 데이터의 이름
+          type: "category",
+          categories: dateData, // 데이터의 이름
         },
         y: {
           label: "Price(million)", // y 축의 라벨
         },
+      },
+      size: {
+        // 차트의 가로 및 세로 길이 지정
+        height: 260,
       },
     });
     return () => {
@@ -35,6 +43,7 @@ function Chart({chartData}: any) {
 
   return (
     <div>
+      <DateBox>{format(new Date(), "yyyy.MM.dd")}</DateBox>
       <ChartWrap id="chart" style={{color: "white"}}></ChartWrap>
     </div>
   );
@@ -42,4 +51,8 @@ function Chart({chartData}: any) {
 
 export default Chart;
 
+const DateBox = styled.div`
+  display: flex;
+  justify-content: end;
+`;
 const ChartWrap = styled.div``;
